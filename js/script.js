b87549aa20b88871567aaec9de59b6dc33e9f89e
Ali Sadeghi <ali909392@gmail.com>
2023-02-21 21:22:11 +0330

    Add ball and make shoot function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 const leg = document.querySelector(".game-box__sticky-man-leg");
+const ball = document.querySelector(".game-box__ball");
 
 let scrolled = 0;
 let lastScrolled = -1;
+let startPosition = -1;
 let startTiming = true;
 let startTime = 0;
+let timeScrolled = 0;
+let score = 0;
 window.addEventListener("scroll", function () {
     scrolled =
         (window.scrollY /
@@ -14,16 +18,19 @@ window.addEventListener("scroll", function () {
         if (startTiming) {
             console.log("Count");
             startTime = Date.parse(new Date());
+            startPosition = scrolled;
             startTiming = false;
         }
         if (scrolled >= "99.9") {
-            console.log(new Date() - startTime);
+            timeScrolled = new Date() - startTime;
+            console.log(timeScrolled);
+            shoot();
             console.log("reset time");
             setTimeout(() => {
                 lastScrolled = -1;
                 startTiming = true;
                 console.log("end of reset");
-            }, 5000);
+            }, 3000);
         }
         lastScrolled = scrolled;
     } else {
@@ -31,3 +38,12 @@ window.addEventListener("scroll", function () {
     }
     leg.style.transform = `translate(-50%, -50%) rotate(-${scrolled * 1.3}deg)`;
 });
+
+function shoot() {
+    score = ((100 - startPosition)*10 / timeScrolled) * 500;
+    console.log("=>", score);
+    ball.style.transform = `translate(-50%, -50%) translate(${score}px ,0px)`;
+    setTimeout(() => {
+        ball.style.transform = "translate(-50%, -50%) translate(0px ,0px)";
+    }, 2500);
+}
