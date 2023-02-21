78320b2c1675cad3dd0418f2f2843475c811dc53
Ali Sadeghi <ali909392@gmail.com>
2023-02-21 18:22:20 +0330

    Add first codes

---
+++ script.js
@@ -0,0 +1,33 @@
+const leg = document.getElementById("leg");
+
+let scrolled = 0;
+let lastScrolled = -1;
+let startTiming = true;
+let startTime = 0;
+window.addEventListener("scroll", function () {
+    scrolled =
+        (window.scrollY /
+            (document.documentElement.scrollHeight - window.innerHeight)) *
+        100;
+
+    if (lastScrolled < scrolled) {
+        if (startTiming) {
+            console.log("Count");
+            startTime = Date.parse(new Date());
+            startTiming = false;
+        }
+        if (scrolled >= "99.9") {
+            console.log(new Date() - startTime);
+            console.log("reset time");
+            setTimeout(() => {
+                lastScrolled = 0;
+                startTiming = true;
+                console.log("end of reset");
+            }, 5000);
+        }
+        lastScrolled = scrolled;
+    } else {
+        startTiming = true;
+    }
+    leg.style.transform = `translate(-50%, -50%) rotate(-${scrolled * 1.3}deg)`;
+});
