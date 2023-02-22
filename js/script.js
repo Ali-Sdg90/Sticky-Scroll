f14d9086c14ef2d035121e3b073f9ccee6337b50
Ali Sadeghi <ali909392@gmail.com>
2023-02-22 12:27:58 +0330

    Add scores / improve scrollbar / change reset time / add localStorge

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
 const leg = document.querySelector(".game-box__sticky-man-leg");
 const ball = document.querySelector(".game-box__ball");
+const highScore = document.getElementById("scores__highest");
+const currentScore = document.getElementById("scores__current");
+const saveHighest = localStorage.getItem("stickManHighest");
 
 let scrolled = 0;
 let lastScrolled = -1;
 let startPosition = -1;
 let startTiming = true;
 let startTime = 0;
-let timeScrolled = 0;
 let score = 0;
+let highScoreValue = 0;
+
+if (saveHighest) {
+    highScore.value = saveHighest;
+    highScoreValue = saveHighest;
+}
+
 window.addEventListener("scroll", function () {
     scrolled =
         (window.scrollY /
@@ -21,9 +30,12 @@ window.addEventListener("scroll", function () {
             startPosition = scrolled;
             startTiming = false;
         }
-        if (scrolled >= "99.9") {
-            timeScrolled = new Date() - startTime;
-            score = (((100 - startPosition) * 10) / timeScrolled) * 500;
+
+        if (scrolled >= 99.9) {
+            score = (
+                (((100 - startPosition) * 10) / (new Date() - startTime)) *
+                500
+            ).toFixed(2);
             if (score > 0) {
                 console.log("=>", score);
 
@@ -31,14 +43,28 @@ window.addEventListener("scroll", function () {
                 setTimeout(() => {
                     ball.style.transform =
                         "translate(-50%, -50%) translate(0px ,0px)";
-                }, 1500);
+                }, 1000);
 
                 console.log("reset time");
                 setTimeout(() => {
                     lastScrolled = -1;
                     startTiming = true;
                     console.log("end of reset");
-                }, 2000);
+                }, 1500);
+                currentScore.value = score;
+                console.log(
+                    score > highScoreValue,
+                    score,
+                    ">",
+                    highScoreValue,
+                    typeof score,
+                    typeof highScoreValue
+                );
+                if (Number(score) > Number(highScoreValue)) {
+                    highScoreValue = score;
+                    highScore.value = score;
+                    localStorage.setItem("stickManHighest", score);
+                }
                 window.scrollTo({
                     top:
                         Math.random() *
@@ -55,12 +81,4 @@ window.addEventListener("scroll", function () {
     }
     leg.style.transform = `translate(-50%, -50%) rotate(-${scrolled * 1.3}deg)`;
 });
-
-// function shoot() {
-//     score = (((100 - startPosition) * 10) / timeScrolled) * 500;
-//     console.log("=>", score);
-//     ball.style.transform = `translate(-50%, -50%) translate(${score}px ,0px)`;
-//     setTimeout(() => {
-//         ball.style.transform = "translate(-50%, -50%) translate(0px ,0px)";
-//     }, 2500);
-// }
+// localStorage.clear();
